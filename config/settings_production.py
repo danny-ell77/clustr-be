42821221d93477b94848ff45ff85230650ed7d5b
Daniel Olah <danielolahskybrow77@gmail.com>
2025-12-18 16:19:05 +0100

    added db connection timeout to speed up requests

--- config/settings_production.py
+++ config/settings_production.py
@@ -26,7 +26,10 @@ DATABASES = {
         "PASSWORD": os.environ.get("DB_PASSWORD", ""),
         "HOST": os.environ.get("DB_HOST", "localhost"),
         "PORT": os.environ.get("DB_PORT", "5432"),
-        "CONN_MAX_AGE": int(os.environ.get("DB_CONN_MAX_AGE", 600)),
+        "CONN_MAX_AGE": 600,
+        "OPTIONS": {
+            "connect_timeout": 10,
+        },
     }
 }
 
