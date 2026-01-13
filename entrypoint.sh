7ffae8baf3e99a2fa088acd1776e2df5c5dc364e
Daniel Olah <danielolahskybrow77@gmail.com>
2026-01-13 04:59:42 +0100

    moved initialization to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,14 @@ set -e
 echo "Ensuring database exists..."
 python scripts/ensure_db_exists.py
 
+# Run migrations
+echo "Running migrations..."
+python manage.py migrate --no-input
+
+# Load initial data if database is empty
+echo "Checking initial data..."
+python scripts/load_initial_data.py
+
 # Start the application
 echo "Starting application..."
 exec "$@"
