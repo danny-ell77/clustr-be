548cdc7385a9be31cb44eaa12bf08e280f0cd753
Aghahowa Jeffrey <aghahowaj01@gmail.com>
2026-06-12 04:50:12 +0100

    chore: added docker compose for dev testing

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "#zeq)d@+g5(i3it%0ehxv$+v*#czxw6w)s$5idb_om)1dtq-9!"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.getenv("DEBUG", "1")))
 
-ALLOWED_HOSTS = ["testserver"]
+ALLOWED_HOSTS = ["testserver", *os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")]
 
 
 # Application definition
